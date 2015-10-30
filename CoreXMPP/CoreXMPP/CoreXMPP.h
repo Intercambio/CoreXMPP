45893ded1daf47b67ffe578540217920f5da6b3a
Tobias Kräntzer <info@tobias-kraentzer.de>
2015-10-30 21:53:19 +0100

    Initial Commit

---
+++ CoreXMPP/CoreXMPP/CoreXMPP.h
@@ -0,0 +1,17 @@
+//
+//  CoreXMPP.h
+//  CoreXMPP
+//
+//  Created by Tobias Kräntzer on 30.10.15.
+//  Copyright © 2015 Tobias Kräntzer. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for CoreXMPP.
+FOUNDATION_EXPORT double CoreXMPPVersionNumber;
+
+//! Project version string for CoreXMPP.
+FOUNDATION_EXPORT const unsigned char CoreXMPPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreXMPP/PublicHeader.h>
