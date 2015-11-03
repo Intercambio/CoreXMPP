d203c21480663456fc949cd503629ab6745f9ca8
Tobias Kräntzer <info@tobias-kraentzer.de>
2015-11-03 17:45:16 +0100

    Add sub project for a test server

---
+++ server/mix.exs
@@ -0,0 +1,20 @@
+defmodule Ejabberd.Server.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :xmpp_server,
+     version: "0.0.1",
+     elixir: "~> 1.1",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger, :ejabberd]]
+  end
+  
+  defp deps do
+    [{:ejabberd, github: "processone/ejabberd", tag: "15.10"}]
+  end
+end
