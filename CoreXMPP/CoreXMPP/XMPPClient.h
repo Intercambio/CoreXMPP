f88bc98cd48bd590d7a5c268d70fd2c4cc13270e
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-01-07 11:01:37 +0100

    Define API for XMPPClient

---
+++ CoreXMPP/CoreXMPP/XMPPClient.h
@@ -0,0 +1,55 @@
+//
+//  XMPPClient.h
+//  CoreXMPP
+//
+//  Created by Tobias Kräntzer on 06.01.16.
+//  Copyright © 2016 Tobias Kräntzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <PureXML/PureXML.h>
+
+typedef NS_ENUM(NSUInteger, XMPPClientState) {
+    XMPPClientStateDisconnected,
+    XMPPClientStateConnecting,
+    XMPPClientStateConnected,
+    XMPPClientStateNegotiating,
+    XMPPClientStateEstablished,
+    XMPPClientStateDisconnecting
+};
+
+@class XMPPClient;
+
+@protocol XMPPClientDelegate <NSObject>
+@optional
+- (void)clientDidConnect:(XMPPClient *)client;
+- (void)clientDidDisconnect:(XMPPClient *)client;
+- (void)client:(XMPPClient *)client didFailWithError:(NSError *)error;
+- (void)client:(XMPPClient *)client didReceiveStanza:(PXElement *)stanza;
+@end
+
+@interface XMPPClient : NSObject
+
+#pragma mark Life-cycle
+- (instancetype)initWithHostname:(NSString *)hostname
+                         options:(NSDictionary *)options;
+
+#pragma mark Delegate
+@property (nonatomic, weak) id<XMPPClientDelegate> delegate;
+@property (nonatomic, strong) dispatch_queue_t delegateQueue;
+
+#pragma mark Properties
+@property (nonatomic, readonly) NSString *hostname;
+@property (nonatomic, readonly) NSDictionary *options;
+
+#pragma mark State
+@property (nonatomic, readonly) XMPPClientState state;
+
+#pragma mark Manage Client
+- (void)connect;
+- (void)disconnect;
+
+#pragma mark Sending Stanza
+- (void)sendStanza:(PXElement *)stanza;
+
+@end
---
+++ CoreXMPP/CoreXMPP/XMPPClient.m
@@ -0,0 +1,94 @@
+//
+//  XMPPClient.m
+//  CoreXMPP
+//
+//  Created by Tobias Kräntzer on 06.01.16.
+//  Copyright © 2016 Tobias Kräntzer. All rights reserved.
+//
+
+#import "XMPPWebsocketStream.h"
+
+#import "XMPPClient.h"
+
+@interface XMPPClient () <XMPPStreamDelegate> {
+    dispatch_queue_t _operationQueue;
+    XMPPClientState _state;
+    XMPPWebsocketStream *_stream;
+}
+
+@end
+
+@implementation XMPPClient
+
+#pragma mark Life-cycle
+
+- (instancetype)initWithHostname:(NSString *)hostname
+                         options:(NSDictionary *)options
+{
+    self = [super init];
+    if (self) {
+        _operationQueue = dispatch_queue_create("XMPPClient", DISPATCH_QUEUE_SERIAL);
+        _stream = [[XMPPWebsocketStream alloc] initWithHostname:hostname options:options];
+        _stream.delegateQueue = _operationQueue;
+        _stream.delegate = self;
+        _state = XMPPClientStateDisconnected;
+    }
+    return self;
+}
+
+#pragma mark Properties
+
+- (NSString *)hostname
+{
+    return _stream.hostname;
+}
+
+- (NSDictionary *)options
+{
+    return _stream.options;
+}
+
+#pragma mark Manage Client
+
+- (void)connect
+{
+    dispatch_async(_operationQueue, ^{
+        
+    });
+}
+
+- (void)disconnect
+{
+    dispatch_async(_operationQueue, ^{
+        
+    });
+}
+
+#pragma mark Sending Stanza
+
+- (void)sendStanza:(PXElement *)stanza
+{
+    dispatch_async(_operationQueue, ^{
+        
+    });
+}
+
+#pragma mark XMPPStreamDelegate
+
+- (void)stream:(XMPPStream *)stream didOpenToHost:(NSString *)hostname withStreamId:(NSString *)streamId
+{
+}
+
+- (void)stream:(XMPPStream *)stream didReceiveElement:(PXElement *)element
+{
+}
+
+- (void)stream:(XMPPStream *)stream didFailWithError:(NSError *)error
+{
+}
+
+- (void)streamDidClose:(XMPPStream *)stream
+{
+}
+
+@end
