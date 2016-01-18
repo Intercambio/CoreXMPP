57031d8cc54d58b27142fa3601357147ac5fcf6a
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-01-18 09:44:02 +0100

    Add logging facility

--- CoreXMPP/CoreXMPP/XMPPClient.m
+++ CoreXMPP/CoreXMPP/XMPPClient.m
@@ -6,6 +6,14 @@
 //  Copyright © 2016 Tobias Kräntzer. All rights reserved.
 //
 
+#import <CocoaLumberjack/CocoaLumberjack.h>
+
+#ifdef DEBUG
+static const DDLogLevel ddLogLevel = DDLogLevelVerbose;
+#else
+static const DDLogLevel ddLogLevel = DDLogLevelWarn;
+#endif
+
 #import "XMPPWebsocketStream.h"
 #import "XMPPStreamFeature.h"
 #import "XMPPStreamFeatureSASL.h"
@@ -159,9 +167,7 @@ NSString *const XMPPClientOptionsResourceKey = @"XMPPClientOptionsResourceKey";
         _state = XMPPClientStateConnecting;
         _negotiatedFeatures = @[];
 
-#ifdef DEBUG
-        NSLog(@"Supported Features: %@", [[self class] registeredStreamFeatures]);
-#endif
+        DDLogDebug(@"Supported Features: %@", [[self class] registeredStreamFeatures]);
 
         [_stream open];
     });
@@ -215,18 +221,16 @@ NSString *const XMPPClientOptionsResourceKey = @"XMPPClientOptionsResourceKey";
             _currentFeature = feature;
             _currentFeature.queue = _operationQueue;
             _currentFeature.delegate = self;
-
-#ifdef DEBUG
-            NSLog(@"Begin negotiation of feature: (%@, %@)", configuration.root.namespace, configuration.root.name);
-#endif
+            
+            DDLogDebug(@"Begin negotiation of feature: (%@, %@)", configuration.root.namespace, configuration.root.name);
 
             [_currentFeature beginNegotiationWithHostname:self.hostname
                                                   options:nil];
 
         } else {
-#ifdef DEBUG
-            NSLog(@"Feature not supported: (%@, %@)", configuration.root.namespace, configuration.root.name);
-#endif
+            
+            DDLogDebug(@"Feature not supported: (%@, %@)", configuration.root.namespace, configuration.root.name);
+            
             [self xmpp_negotiateNextFeature];
         }
 
