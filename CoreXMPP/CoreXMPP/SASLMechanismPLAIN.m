5ce925e68cc577e87789139b184adf823bea9263
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-01-19 12:01:44 +0100

    Extend logging for SASLMechanismPLAIN

--- CoreXMPP/CoreXMPP/SASLMechanismPLAIN.m
+++ CoreXMPP/CoreXMPP/SASLMechanismPLAIN.m
@@ -6,6 +6,10 @@
 //  Copyright © 2016 Tobias Kräntzer. All rights reserved.
 //
 
+#import <CocoaLumberjack/CocoaLumberjack.h>
+
+static DDLogLevel ddLogLevel = DDLogLevelWarning;
+
 #import "SASLMechanismPLAIN.h"
 
 @interface SASLMechanismPLAIN () {
@@ -22,6 +26,16 @@
 {
     [SASLMechanism registerMechanismClass:self
                          forMechanismName:[self name]];
+}
+
++ (DDLogLevel)ddLogLevel
+{
+    return ddLogLevel;
+}
+
++ (void)ddSetLogLevel:(DDLogLevel)logLevel
+{
+    ddLogLevel = logLevel;
 }
 
 #pragma mark Mechanism Name
@@ -47,7 +61,10 @@
 - (void)beginAuthenticationExchangeWithHostname:(NSString *)hostname responseHandler:(void (^)(NSData *, BOOL))responseHandler
 {
     dispatch_async(_queue, ^{
+        _hostname = hostname;
         _responseHandler = responseHandler;
+
+        DDLogInfo(@"Begin authentication for '%@'.", hostname);
 
         dispatch_queue_t queue = self.delegateQueue ?: dispatch_get_main_queue();
         dispatch_async(queue, ^{
@@ -65,6 +82,8 @@
         if (_responseHandler) {
             if (username && password) {
 
+                DDLogInfo(@"Authenticate with username (%@) and password (...) for '%@'.", username, _hostname);
+
                 unsigned short nul[] = {0};
 
                 NSMutableData *response = [[NSMutableData alloc] init];
@@ -77,6 +96,9 @@
 
                 _responseHandler(response, NO);
             } else {
+
+                DDLogWarn(@"Abort authentication for '%@', because either username or password is nil.", _hostname);
+
                 _responseHandler(nil, YES);
             }
             _responseHandler = nil;
@@ -88,6 +110,9 @@
 {
     dispatch_async(_queue, ^{
         if (_responseHandler) {
+
+            DDLogWarn(@"Abort authentication for '%@'.", _hostname);
+
             _responseHandler(nil, YES);
             _responseHandler = nil;
         }
