0bfaa99a185b1784d57c2248305d03daa2ec2b78
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-01-24 00:43:36 +0100

    Add additional test for the state handling of websocket streams.

--- CoreXMPP/CoreXMPPTests/XMPPWebsocketStreamTests.m
+++ CoreXMPP/CoreXMPPTests/XMPPWebsocketStreamTests.m
@@ -30,7 +30,7 @@
     //
     // Set delegate
     //
-    
+
     id<XMPPStreamDelegate> delegate = mockProtocol(@protocol(XMPPStreamDelegate));
     stream.delegate = delegate;
 
@@ -64,6 +64,54 @@
 
     [verifyCount(delegate, times(1)) stream:stream
                               didOpenToHost:equalTo(@"localhost")
+                               withStreamId:notNilValue()];
+
+    [verifyCount(delegate, times(1)) streamDidClose:stream];
+}
+
+- (void)testCloseByRemotePeer
+{
+    // Setup a stream to a unknow host (foo.bar). In this case the server will
+    // send a stream error and will close the stream immediately without
+    // waiting for the closing element by the local peer.
+
+    NSDictionary *options = @{ XMPPWebsocketStreamURLKey : [NSURL URLWithString:@"ws://localhost:5280/xmpp"] };
+    XMPPWebsocketStream *stream = [[XMPPWebsocketStream alloc] initWithHostname:@"foo.bar"
+                                                                        options:options];
+
+    id<XMPPStreamDelegate> delegate = mockProtocol(@protocol(XMPPStreamDelegate));
+    stream.delegate = delegate;
+
+    [givenVoid([delegate stream:stream didReceiveElement:anything()]) willDo:^id(NSInvocation *invocation) {
+
+        PXElement *element = [[invocation mkt_arguments] lastObject];
+        NSError *error = [XMPPClient streamErrorFromElement:element];
+
+        assertThat(error, notNilValue());
+        assertThat(error.domain, equalTo(XMPPClientStreamErrorDomain));
+        assertThatInteger(error.code, equalToInteger(XMPPClientStreamErrorCodeHostUnknown));
+
+        // If the client detects an stream error, it will clsoe the stream. This should not result
+        // into timing issues with the server (who will be the first closing the stream).
+
+        [stream close];
+        return nil;
+    }];
+
+    XCTestExpectation *waitForClose = [self expectationWithDescription:@"Close"];
+    [givenVoid([delegate streamDidClose:stream]) willDo:^id(NSInvocation *invocation) {
+        [waitForClose fulfill];
+        return nil;
+    }];
+    [stream open];
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
+
+    //
+    // Verify delegate calles
+    //
+
+    [verifyCount(delegate, times(1)) stream:stream
+                              didOpenToHost:anything()
                                withStreamId:notNilValue()];
 
     [verifyCount(delegate, times(1)) streamDidClose:stream];
